09b3d21c47a7547b229ee524cfc6a2647b6225f0
Daniel <daniel.palacz@gmail.com>
2025-03-05 22:48:06 +0100

    refactores Python question-like notes, adds unpacking_puzzle

---
+++ python_deepness/standard_things_not_always_used/unpacking_puzzle.py
@@ -0,0 +1,13 @@
+
+def func():
+    a, *b, c = ["Tony", "Phony", "Pony"]
+    return "Phony" in [b] or a[:]
+
+OUT = func() # "Tony" as b = ["Phony"] => "Phony" in [["Phony"]] => False
+
+
+# def func2():
+#     a, b, c = ["Tony", "Phony", "Pony"]
+#     return "Phony" in [b] or a[:]
+#
+# OUT2 = func2() # True
