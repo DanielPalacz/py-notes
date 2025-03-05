80ce372292045102c29dc4b588adc3e4d11aff95
Daniel <daniel.palacz@gmail.com>
2025-03-05 20:26:41 +0100

    adds few JS notes/observations

--- index-django-javascript-files/basics_script.js
+++ index-django-javascript-files/basics_script.js
@@ -1,6 +1,8 @@
 function generateRandomNumber() {
+  x = 101; // 'let', 'const', 'var' - no? then x goes to global;
+
   let randomNumber = Math.floor(Math.random() * 100);
-  console.log(`JavaScript z zewnętrznego pliku 2 [${Date.now()}][${randomNumber}]`);
+  console.log(`JavaScript z zewnętrznego pliku 2 [${Date.now()}][randomNumber=${randomNumber}]`);
   document.addEventListener("DOMContentLoaded", function () {
     document.body.innerHTML += `<p>[JS script external file] H! Random number is: ${randomNumber}</p>`;
   });
