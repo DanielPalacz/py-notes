8c7f1bd3bcb28616c01d0707fbdfada2e4f08cfa
Daniel <daniel.palacz@gmail.com>
2025-05-17 15:02:51 +0200

    updates JS notes

--- index-javascript-files/basics_script_fibonacci.js
+++ index-javascript-files/basics_script_fibonacci.js
@@ -1,16 +1,17 @@
-let a = 0;
-let b = 1;
 
 function runFibonacci(num) {
+  let a = 0;
+  let b = 1;
   while (a < num) {
-    console.log(a); // Wypisuje bieżącą wartość `a`
     let temp = a; // Zapisujemy wartość `a` do zmiennej pomocniczej
     a = b; // Przypisujemy `b` do `a` (przechodzimy na następną liczbę)
     b = temp + b; // Przypisujemy sumę poprzednich dwóch liczb do `b`
   }
-  console.log('Checking this object in global function. This is:', this);
+  // console.log('Checking this object in global function. This is:', this);
+  return b
 }
 
-let x = runFibonacci(200);
+let x = runFibonacci(6);
+
 console.log("runFibonacci returned:", x);
 console.log('Checking this object in global context. This is:', this);
