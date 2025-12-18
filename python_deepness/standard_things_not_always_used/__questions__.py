8ba31cdc25d7b1e5c91a240977b88160d57694ce
Daniel <daniel.palacz@gmail.com>
2025-12-18 15:45:52 +0100

    adds __questions__.py file

---
+++ python_deepness/standard_things_not_always_used/__questions__.py
@@ -0,0 +1,7 @@
+
+Q1 = "Unicode strings (python3). Bytes?"
+Q2 = "Unpacking in Python. How to use? It is known as good practice"
+Q3 = "By using: maketrans and translate (str methods) one can change letters in text (fe. changing small letters to capital)."
+Q4 = "How to operate on sys.stdin?"
+Q5 = "How to redirect sys.stdout?"
+Q6 = "How to configure other than UTF-8 coding?"
