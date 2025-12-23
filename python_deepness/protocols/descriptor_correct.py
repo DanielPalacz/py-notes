5d93b6924ed4f54f63de2a2f1dbf21770ba539b1
Daniel <daniel.palacz@gmail.com>
2025-12-23 09:59:54 +0100

    adds descriptor-like code

---
+++ python_deepness/protocols/descriptor_correct.py
@@ -0,0 +1,27 @@
+class PositiveInt:
+    def __set_name__(self, owner, name):
+        self.name = name
+        # self.private_name = '_' + name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        # print(f"Setting {self.name} = {value}")
+
+        if value < 0:
+            raise ValueError("Value must be positive.")
+
+        instance.__dict__[self.name] = value
+
+class Product:
+    price = PositiveInt()
+    quantity = PositiveInt()
+
+p = Product()
+p.price = 100
+p.quantity = 5
+
+print(p.price, p.quantity)   # 100 5
+
+p.price = -10                # ValueError
