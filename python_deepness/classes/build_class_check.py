b4a4701f7a092cea798d93107ae97003e72b040e
Daniel <daniel.palacz@gmail.com>
2025-12-30 15:07:44 +0100

    adds build_class_check.py

--- python_deepness/classes/build_class_check.py
+++ python_deepness/classes/build_class_check.py
@@ -3,30 +3,34 @@ import dis
 
 from random import randint
 
+
 original_build_class = builtins.__build_class__
+
 
 class TypeDebug(type):
 
     def __init__(cls, what, bases, dct):
-        # print(f"Wywołanie type.__init__: what={what}, bases={bases}, dct={dct}")
-        print(f"Calling type.__init__: what={what}, bases={bases}, dct={dct}")
+        print(f"[TypeDebug][__init__] Calling type.__init__: what={what}, bases={bases}, dct={dct}")
         type.__init__(cls, what, bases, dct)
 
 
     def __new__(cls, *args, **kwargs):
-        print(f"Calling type.__new__: args={args}, kwargs={kwargs}")
-        print(" - locals:", locals())
-        print(" - locals['__class__']:", dir(locals()["__class__"]))
+
+        print()
+
+        print(f"[TypeDebug][__new__] Calling type.__new__: args={args}, kwargs={kwargs}")
+        print("[TypeDebug][__new__] - locals:", locals())
+        print("[TypeDebug][__new__] - locals['__class__']:", dir(locals()["__class__"]))
         return super().__new__(cls, *args, **kwargs)
 
 
     def __prepare__(cls, *args, **kwargs):
-        print(f"Calling type.__prepare__: args={args}, kwargs={kwargs}")
+        print(f"[TypeDebug][__prepare__] Calling type.__prepare__: args={args}, kwargs={kwargs}")
         return {"prepare": 1}
 
 
 def debug_build_class(*args, **kwargs):
-    print(f"Calling __build_class__: args={args}, kwargs={kwargs}")
+    print(f"[debug_build_class] Calling __build_class__: args={args}, kwargs={kwargs}")
     return original_build_class(*args, **kwargs)
 
 
@@ -42,40 +46,23 @@ class MagicRandomNumber:
             raise NotImplemented("Not implemented RandomNumber generation.")
 
     def __set_name__(self, owner, name):
-        print(f" # Calling descriptor_obj.__set_name__(self, owner, name)")
+        print(f" - calling descriptor_obj.__set_name__(self, owner, name)")
         self.method = "random.randint"
 
 
+print("--------------------------------------------------------------------------------------------------------------------------------------------------------")
+
 
 class ClassCreationFlowReview(metaclass=TypeDebug):
-    print()
-    print("Executing code of Class body - start.")
-    print(" * locals() during Class body execution", locals())
-    print("   - dir() during Class body execution", dir())
-
+    print("[ClassCreationFlowReview] Executing code of Class body - start.")
+    print("[ClassCreationFlowReview] locals() during Class body execution", locals())
+    print("[ClassCreationFlowReview] dir() during Class body execution", dir())
 
     r = MagicRandomNumber()
     x = 10
 
-    # @staticmethod
-    # def method():
-    #     return super()  # Using super() triggers adding __classcell__ to Namespace
-
-    print(" * locals() during Class body execution", locals())
-    print(" * Executing code of Class body - end.")
-    print()
-
-
-# print(ClassCreationFlowReview().prepare)
-# print("Magic number is:", ClassCreationFlowReview().r)
-
-# class B(A):
-#     y = 20
-
-# C = type("C", (object,), {"z": 30})
-# print(dir())
-# __builtins__.__build_class__("C", (object,), {"z": 30})
-
+    print("[ClassCreationFlowReview] locals() during Class body execution", locals())
+    print("[ClassCreationFlowReview] Executing code of Class body - end.")
 
 
 # Class creation protocol:
@@ -83,7 +70,6 @@ class ClassCreationFlowReview(metaclass=TypeDebug):
 #  - [metaclass].__prepare__
 #  - executing code of the given Class body (like function type)
 #  - [the given class].__set_name__(cls, descr_name)
-
 #  - [the given class].__new__
 #  - [the given class].__init__
 
