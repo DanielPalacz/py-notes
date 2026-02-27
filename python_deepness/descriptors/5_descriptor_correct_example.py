7750ff25afad7cc43ea3a235584227e0825fc605
Daniel <daniel.palacz@gmail.com>
2026-02-27 15:52:48 +0100

    updates Descriptors

--- python_deepness/descriptors/5_descriptor_correct_example.py
+++ python_deepness/descriptors/5_descriptor_correct_example.py
@@ -1,9 +1,15 @@
+
+
 class PositiveInt:
     def __set_name__(self, owner, name):
         self.name = name
         # self.private_name = '_' + name
 
     def __get__(self, instance, owner):
+
+        if instance is None:
+            return self
+
         return instance.__dict__[self.name]
 
     def __set__(self, instance, value):
@@ -18,10 +24,12 @@ class Product:
     price = PositiveInt()
     quantity = PositiveInt()
 
-p = Product()
-p.price = 100
-p.quantity = 5
 
-print(p.price, p.quantity)   # 100 5
-
-p.price = -10                # ValueError
+#
+# p = Product()
+# p.price = 100
+# p.quantity = 5
+#
+# print(p.price, p.quantity)   # 100 5
+#
+# p.price = -10                # ValueError
