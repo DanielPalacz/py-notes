6c447f20aa5b3a7c58d8ea2ae2d33408a8a1da0f
Daniel <daniel.palacz@gmail.com>
2026-04-13 16:45:03 +0200

    updates sanic lib notes

--- different_libraries/sanic_hello.py
+++ different_libraries/sanic_hello.py
@@ -1,8 +1,15 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json, text
 
 app = Sanic("my-hello-world-app")
+
+@app.get("/")
+async def hello(request):
+    return text("Hello, Sanic!")
 
 @app.route('/abc')
 async def test(request):
     return json({'hello': 'world'})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
