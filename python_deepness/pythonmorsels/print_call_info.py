03ad04e79d13ea8dba4e2e8d3a1c420bbc3e20d2
Daniel <daniel.palacz@gmail.com>
2026-09-15 18:01:32 +0200

    adds print_call_info exercise

---
+++ python_deepness/pythonmorsels/print_call_info.py
@@ -0,0 +1,42 @@
+
+from functools import wraps
+from time import perf_counter
+
+
+def print_call_info(func=None, *, time=True):
+    def actual_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = perf_counter()
+            ret_val = func(*args, **kwargs)
+            runtime = perf_counter() - start
+
+            call = f"{func.__name__}({format_args(args, kwargs)})"
+
+            if time:
+                print(f"{call} returned {ret_val} ({runtime} seconds)")
+            else:
+                print(f"{call} returned {ret_val}")
+
+            return ret_val
+
+        return wrapper
+
+    # @print_call_info
+    if func is not None:
+        return actual_decorator(func)
+
+    # @print_call_info(...)
+    return actual_decorator
+
+
+def format_args(args, kwargs):
+    arg_strings = (
+        repr(a)
+        for a in args
+    )
+    kwarg_strings = (
+        f"{name}={value!r}"
+        for name, value in kwargs.items()
+    )
+    return ", ".join([*arg_strings, *kwarg_strings])
